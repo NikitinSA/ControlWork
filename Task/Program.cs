02268c2cb66f2aff26feb99f5a6a852d390f0565
Савелий <Savka9765@yandex.ru>
2022-09-17 22:27:41 +0300

    Программа формирует из введенного пользователем массива массив из элементов с 3 или меньше символами

---
+++ Task/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayFirst = new string[size];
+string[] resultArray = new string[arrayFirst.Length];
+
+FillArray(arrayFirst, size);
+Console.WriteLine();
+Console.Write("Введенный массив: ");
+PrintArray(arrayFirst);
+Console.WriteLine();
+NumberOfCharacters(arrayFirst, resultArray);
+Console.Write("Массив из строк длина которых меньше или равна 3 символам: ");
+PrintArray(resultArray);
+
+void FillArray(string[] arrayFirst, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите значение элемента массива: ");
+        arrayFirst[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+
+void NumberOfCharacters(string[] arrayFirst, string[] resultArray)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i].Length <= 3)
+        {
+            resultArray[count] = arrayFirst[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
